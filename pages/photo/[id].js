ebc168039575292190138de9d723d72b820e5426
Soyhipst <hello.codehipster@outlook.com>
2022-09-05 23:29:17 -0500

    first commit

---
+++ pages/photo/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import Images from "next/image";
+import Link from "next/link";
+
+export default function photo({ photo }) { }
+
+export async function getStaticProps({params}){
+
+}
+
+export async function getStaticPaths(){
+    
+}
